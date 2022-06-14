b063ce736feea93cf8a4e183c9005e87c551cb65
XuToWei <xw741619720@qq.com>
2022-06-14 20:54:18 +0800

    优化ILRuntime结构

--- Assets/Scripts/Game/ILRuntime/Runtime/IAdapterProperty.cs
+++
@@ -1,9 +0,0 @@
-using ILRuntime.Runtime.Enviorment;
-using ILRuntime.Runtime.Intepreter;
-
-
-public interface IAdapterProperty
-{
-    AppDomain AppDomain { get; set; }
-    ILTypeInstance ILInstance { get; set; }
-}
